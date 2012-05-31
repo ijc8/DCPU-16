07ce5b6573525b35a63a7e4ad57c3b6cf95c0c35
bgs100 <ijcguy1@gmail.com>
2012-05-30 23:24:20 -0400

    Mostly implemented keyboard interrupts (no support for operation 3, set interrupt message, yet).

--- net/ian/dcpu/Keyboard.java
+++ net/ian/dcpu/Keyboard.java
@@ -5,11 +5,18 @@ import java.awt.event.KeyListener;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.ian.dcpu.DCPU.Register;
+
 public class Keyboard extends Hardware implements KeyListener {
 	public static final int ID = 0x30cf7406;
 	public static final int VERSION = 1;
 	
 	private static Map<Integer, Integer> keyMap = new HashMap<>();
+	private char[] keyring = new char[64];
+	private int keyPushPtr = 0;
+	private int keyAccessPtr = 0;
+	// Max key value seems to be 145.
+	private boolean[] keyStates = new boolean[146];
 	
 	DCPU cpu;
 	
@@ -37,23 +44,25 @@ public class Keyboard extends Hardware implements KeyListener {
 		return keyMap.containsKey(key) ? keyMap.get(key) : -1;
 	}
 	
-	private void addKey(int key) {
-		int tmp = cpu.memory[0x9010].value - 0x9000 + 1;
-		cpu.memory[0x9010].value = (char)(((tmp < 0 ? 0 : tmp) % 0xf) + 0x9000);
-		cpu.memory[cpu.memory[0x9010].value].value = (char)key;
+	private void addKey(char key) {
+		keyring[keyPushPtr++] = key;
+		keyPushPtr &= 64;
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = mapKey(e.getKeyCode());
-		if (key > 0)
-			addKey(key);
+		if (key == -1) return;
+		addKey((char)key);
+		keyStates[key] = true;
 		System.err.printf("Key press: %d (dcpu: %d)\n", e.getKeyCode(), key);
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int key = mapKey(e.getKeyCode());
+		if (key == -1) return;
+		keyStates[key] = false;
 		System.err.printf("Key release: %d (dcpu: %d)\n", e.getKeyCode(), key);
 	}
 	
@@ -63,5 +72,31 @@ public class Keyboard extends Hardware implements KeyListener {
 			addKey(e.getKeyChar());
 			System.err.printf("Key typed: %d = %c\n", (int)e.getKeyChar(), e.getKeyChar());
 		}
+	}
+	
+	public void interrupt() {
+		int b = cpu.getRegister(Register.B).value;
+		int c = cpu.getRegister(Register.C).value;
+		switch (cpu.getRegister(Register.A).value) {
+		case 0: // Clear buffer
+			for (int i = 0; i < keyring.length; i++)
+				keyring[i] = 0;
+			keyPushPtr = 0;
+			keyAccessPtr = 0;
+			break;
+		case 1: // Store next key in C (or 0 if the buffer's empty).
+			if ((c = keyring[keyAccessPtr]) != 0) {
+				keyring[keyAccessPtr++] = 0;
+				keyAccessPtr &= 64;
+			}
+			break;
+		case 2: // Set C to 1 if key specified by B is pressed, 0 otherwise.
+			c = keyStates[b] ? 1 : 0;
+			break;
+		case 3: // If B is 0, disable interrupts. Otherwise enable them with message B.
+			// TODO!
+			break;
+		}
+		cpu.getRegister(Register.C).set(c);
 	}
 }
